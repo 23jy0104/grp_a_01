d127fd18fd7986e1d08c96adbefbcff3bc1e7e07
23jy0114 <23jy0114@jec.ac.jp>
2025-02-12 17:02:56 +0900

    追加

--- CarShareHome/src/main/java/model/Reservation.java
+++ CarShareHome/src/main/java/model/Reservation.java
@@ -1,7 +1,7 @@
 package model;
 
 import java.io.Serializable;
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
 public class Reservation implements Serializable {
 	private String reservationId;
