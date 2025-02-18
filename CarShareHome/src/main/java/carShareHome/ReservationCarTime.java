bd69b1f34e4887375db18b45b24fc16d2e26a986
23jy0104 <23jy0104@jec.ac.jp>
2025-02-18 15:54:07 +0900

    更新

--- CarShareHome/src/main/java/carShareHome/ReservationCarTime.java
+++ CarShareHome/src/main/java/carShareHome/ReservationCarTime.java
@@ -1,6 +1,11 @@
 package carShareHome;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,6 +34,38 @@ public class ReservationCarTime extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8"); 
+        String startdate =request.getParameter("startCalender");
+        String starttime =request.getParameter("startTime");
+        String startDate =startdate+" "+starttime;
+        String enddate =request.getParameter("endCalender");
+        String endtime =request.getParameter("endTime");
+        String endDate =enddate+" "+endtime;
+        String stationid =request.getParameter("stationId");
+        String makername =request.getParameter("mekerName");
+        
+        Connection con =null;
+        PreparedStatement ps =null;
+        ResultSet rs =null;
+        
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://10.64.144.5:3306/23jya01";
+			String user = "23jya01";
+			String pass = "23jya01";
+
+			// データベース接続
+			con = DriverManager.getConnection(url, user, pass);
+			
+			String sql ="select ";
+			
+			ps =con.prepareStatement(sql);
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 
 }
